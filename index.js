03b3c1dd828e03f03864a63c8f3a9b34a31c4d19
nikolai-petukhov <nikolaipetukhov@gmai.com>
2022-11-09 21:48:25 +0300

    deploy heroku

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ morgan.token('requestBody', (request, response) => {
 app.use(express.json());
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :requestBody'));
 app.use(cors());
+app.use(express.static('build'))
 
 const persons = [
     { 
